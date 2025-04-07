9674e3e90529f42ae7b86ef03ae0c27eda634eb5
o0anais0o <choisy_anais@hotmail.fr>
2025-04-07 17:34:00 +0200

    creation of the files 1-main.c and 1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_insert_left - inserts a node as the left-child of another node
+* @parent: a pointer to the node to insert the left-child in
+* @value: the value to store in the new node
+*
+* Return: a pointer to the created node, NULL on failure or if parent is NULL
+*/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t)); /*Memory allocation for a new node*/
+
+	if (new_node == NULL) /*Check if the allocation was successful*/
+		return (NULL); /*Error handling if malloc returns NULL*/
+
+	new_node->n = value; /*Assign value to the node's data field.*/
+	new_node->parent = parent; /*Define the node's parent*/
+	new_node->left = NULL; /*Initializes left pointers to NULL (no children)*/
+	new_node->right = NULL; /*Initializes right pointers to NULL (no children)*/
+
+	if (parent->left == NULL)
+	{
+		parent->left = new_node; /*Returns the pointer to the node if successful*/
+		new_node->left = NULL; /*Returns NULL if the allocation fails*/
+	}
+
+	return (new_node); /*Returns ne_node*/
+}
