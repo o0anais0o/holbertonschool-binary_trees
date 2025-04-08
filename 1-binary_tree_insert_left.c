5d6bff67c0cb6df057dc3092512b5775d24cb445
o0anais0o <choisy_anais@hotmail.fr>
2025-04-08 10:26:02 +0200

    Modified the 1-binary_tree_insert_left.c file.

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,21 +12,24 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t)); /*Memory allocation for a new node*/
+	if (parent == NULL) /*Check if parent is NULL*/
+	return (NULL);
 
+	new_node = malloc(sizeof(binary_tree_t)); /*Memory allocation for a new node*/
 	if (new_node == NULL) /*Check if the allocation was successful*/
 		return (NULL); /*Error handling if malloc returns NULL*/
 
 	new_node->n = value; /*Assign value to the node's data field.*/
 	new_node->parent = parent; /*Define the node's parent*/
-	new_node->left = NULL; /*Initializes left pointers to NULL (no children)*/
-	new_node->right = NULL; /*Initializes right pointers to NULL (no children)*/
+	new_node->right = NULL; /* No right child for this new node */
+	new_node->left = parent->left; /*Point to current left child of parent*/
 
-	if (parent->left == NULL)
-	{
-		parent->left = new_node; /*Returns the pointer to the node if successful*/
-		new_node->left = NULL; /*Returns NULL if the allocation fails*/
-	}
+	/*Handle case where parent already has a left child*/
+	if (parent->left != NULL)
+	parent->left->parent = new_node;
 
-	return (new_node); /*Returns ne_node*/
+	/*Update parent's left pointer to point to the new node*/
+	parent->left = new_node;
+
+	return (new_node); /*Returns new_node*/
 }
